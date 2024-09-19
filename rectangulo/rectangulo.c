236dc0d9f05e8fa7141639604ac5c5696102c5d8
salarconr03 <salarconr2100@alumno.ipn.mx>
2024-09-19 07:38:18 -0600

    Práctica uno y dos

---
+++ rectangulo/rectangulo.c
@@ -0,0 +1,32 @@
+#include <stdio.h> 
+#include <math.h>
+
+void main(){
+    //variables para el rectangulo
+    float base = 0;
+    float altura = 0;
+    float area = 0;
+    float perimetro = 0;
+    //variables para la distancia euclidiana
+    float x1 = 0;
+    float y1 = 0;
+    float x2 = 0;
+    float y2 = 0;
+    float distancia = 0;
+
+    //rectangulo
+    printf("Ingresa la base y la altura del rectangulo. \n");
+    scanf("%f %f", &base, &altura);
+    area = base * altura;
+    perimetro = 2 * (base + altura);
+    printf("El area del rectangulo es:  %.2f ", area);
+    printf("El perimetro del rectangulo es:  %.2f" , perimetro);
+    //distancia euclidiana
+    printf("\nIngresa las coordenadas del primer punto en formato (x,y). \n");
+    scanf("%f %f", &x1, &y1);
+    printf("Ingresa las coordenadas del segundo punto en formato (x,y). \n");
+    scanf("%f %f", &x2, &y2);
+    distancia = pow(pow(x2-x1, 2)+pow(y2-y1, 2), 0.5);
+    printf("La distancia entre los dos puntos es: %.2f", distancia);
+
+}
---
+++ uno/uno.c
@@ -0,0 +1,19 @@
+#include <stdio.h> 
+
+//Se definen constantes que vayan a usar en el programa
+#define PI 3.14159
+#define NOMBRE_INSTITUCION "Escuela de programación"
+#define MAX_ESTUDIANTES 30
+
+//Se define la función principal o main del programa
+void main(){
+    //Se defininen variables para su posterior uso en el programa.
+    int numEnt= 2;
+    float numFlo = 2.5;
+    double numDob = 5.6312;
+    char letraB = 'b';
+    int bool = 1;
+    
+    //se usa printf para imprimir un mensaje en la consola
+    printf("\tHola\n\t\"pero sin prisa\"\n\"o no\ncomo quieras\"");
+}
